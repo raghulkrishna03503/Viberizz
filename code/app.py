bfce5559840e86bba8baceaab6b40ae7d6079dd1
raghulkrishna03503 <108167463+raghulkrishna03503@users.noreply.github.com>
2024-04-14 13:11:44 +0530

    Update app.py and index.html

--- code/app.py
+++ code/app.py
@@ -42,35 +42,14 @@ def recommend(song):
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        selected_movie = request.form['song']
+        recommended_music_names, recommended_music_posters = recommend(selected_movie)
+        return render_template('index.html', recommended_music_names=recommended_music_names, recommended_music_posters=recommended_music_posters)
+    else:
+        music = pickle.load(open('model/df.pkl','rb'))
+        music_list = music['song'].values
+        return render_template('index.html', music_list=music_list)
 
 if __name__ == '__main__':
     app.run()
-
-# st.header('Viberizz')
-
-# music_list = music['song'].values
-# selected_movie = st.selectbox(
-#     "Type or select a song from the dropdown",
-#     music_list
-# )
-
-# if st.button('Show Recommendation'):
-#     recommended_music_names,recommended_music_posters = recommend(selected_movie)
-#     col1, col2, col3, col4, col5= st.columns(5)
-#     with col1:
-#         st.text(recommended_music_names[0])
-#         st.image(recommended_music_posters[0])
-#     with col2:
-#         st.text(recommended_music_names[1])
-#         st.image(recommended_music_posters[1])
-
-#     with col3:
-#         st.text(recommended_music_names[2])
-#         st.image(recommended_music_posters[2])
-#     with col4:
-#         st.text(recommended_music_names[3])
-#         st.image(recommended_music_posters[3])
-#     with col5:
-#         st.text(recommended_music_names[4])
-#         st.image(recommended_music_posters[4])
